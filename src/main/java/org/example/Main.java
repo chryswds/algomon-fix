74eb5584ddf6c1ce287eed6b90a24de02b91f6fc
chryswds <dybas.chrys@gmail.com>
2024-11-11 11:15:43 +0000

    menu creation - user can choose which algomon they want to use - only one option so far

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,36 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        Firezard fire = new Firezard();
+        chooseOpt();
 
 
-        fire.turnAction();
+    }
 
 
+    public static void chooseOpt() {
+        Scanner myKB = new Scanner(System.in);
+        System.out.println("choose an algomon\n"
+                + "1) firezard\n"
+                + "2) Leave program\n");
+        int opt = myKB.nextInt();
 
-
-
-
+        do {
+            switch (opt) {
+                case 1:
+                    Firezard fire = new Firezard();
+                    fire.turnAction();
+                    break;
+                case 2:
+                    System.out.println("you lef");
+                    break;
+            }
+        } while (opt <= 1 || opt > 2);
 
     }
 }
